255d2c2ee668c09601676b31ef4292403b5d6fcf
Suraj Khopkar <140323739+surajskhopkar@users.noreply.github.com>
2024-06-11 21:32:43 +0530

    java script basic data types and variables

--- tests/Basics1.js
+++ tests/Basics1.js
@@ -1,1 +1,27 @@
-abababab
+console.log("Hello World")
+
+let a=4.51
+
+let b=5.44
+
+console.log(a)
+console.log(b)
+console.log(typeof(a))
+
+var c = "Palywright"
+console.log(typeof(c))
+
+let d = true
+console.log(typeof(d))
+console.log(!d)
+
+c = a+b
+console.log(c)
+
+const z = 51
+z = 50
+console.log(z)
+
+
+
+
