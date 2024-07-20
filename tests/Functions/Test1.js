b95c95449b47fa32953edf7a7bbaecb27862b16f
Suraj Khopkar <140323739+surajskhopkar@users.noreply.github.com>
2024-07-20 10:12:27 +0530

    aaaaa

---
+++ tests/Functions/Test1.js
@@ -0,0 +1,8 @@
+//block of code
+
+function add(a,b){
+    return a+b
+}
+
+let res = add(3,5)
+console.log(res)
---
+++ tests/Functions/Test2.js
@@ -0,0 +1,13 @@
+// Anonymous functions => do not have any name => Function expression
+
+let sum = function(c,d){
+    return c+d
+}
+
+let sum1 =(c,d) => c+d
+console.log(sum1(3,4))
+
+
+
+
+
